343412f6fe8a0ca46c53c4620d47e9d7e4c24fa0
Ketan Parmar <kp3849668@gmail.com>
2022-11-01 12:20:51 +0530

    second commit in git

---
+++ src/com/datatype/Typecasting.java
@@ -0,0 +1,8 @@
+package com.datatype;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        float num = 573723.7337638f;
+        System.out.println((int)num);
+    }
+}
