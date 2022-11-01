68b7dc025656430554b0d685959647a96d30bec7
Ketan Parmar <kp3849668@gmail.com>
2022-11-01 11:33:05 +0530

    second commit in git

---
+++ src/com/datatype/Main.java
@@ -0,0 +1,18 @@
+package com.datatype;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 45;
+        float b = 43.849f;
+        int c = 'k';
+        short d = 25;
+        byte e = 32;
+        long f = 588847879433001l;
+        double g = 443.8319304831357;
+
+        double ans = (a+b)/(c*d)-(e-f)*(g-a)+(a/c);
+        System.out.println("This expression answer= "+ans);
+
+
+    }
+}
