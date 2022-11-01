d6027f4bb176a0255e3a5ee427b60301a79b2c60
Ketan Parmar <kp3849668@gmail.com>
2022-11-01 12:27:58 +0530

    second commit in git

--- src/com/datatype/Typecasting.java
+++ src/com/datatype/Typecasting.java
@@ -4,5 +4,9 @@ public class Typecasting {
     public static void main(String[] args) {
         float num = 573723.7337638f;
         System.out.println((int)num);
+
+        int number =  '$';
+        System.out.println(number);
+        System.out.println("🎅");
     }
 }
