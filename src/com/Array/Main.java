011431caaf5192abe24106ecf87152887f7a3187
Ketan Parmar <kp3849668@gmail.com>
2022-11-03 11:25:21 +0530

    third commit in git

---
+++ src/com/Array/Main.java
@@ -0,0 +1,19 @@
+package com.Array;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Array:- Basically array is collection of same data item
+
+              syntax:-
+              data_type[] variable_name = new data_type[size];
+         */
+
+        int[] arr = new int[5];
+        // or
+        int[] arr1; // declaration of array. arr is getting defined in the stack.
+        arr1 = new int[3]; // actually here object is being created in the memory(heap).
+
+    }
+}
