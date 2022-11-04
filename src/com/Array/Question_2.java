e325b5791c9b1254341f4897a22a2101ac7e63ba
Ketan Parmar <kp3849668@gmail.com>
2022-11-04 21:46:28 +0530

    third commit in git

--- src/com/Array/Question_2.java
+++ src/com/Array/Question_2.java
@@ -17,21 +17,30 @@ public class Question_2 {
         System.out.println("The Maximum value in Array is= "+ans1);
 
         int ans = max1(arr,3,8);
-        System.out.println("The Maximum value in Array is= "+ans);
+        System.out.println("The Maximum value in given range in Array is= "+ans);
+
+        int ans2 = min(arr);
+        System.out.println("The Minimum value in Array is= "+ans2);
+
+        int ans3 = min1(arr,3,8);
+        System.out.println("The Minimum value in given range in Array is= "+ans3);
+
+
+//        System.out.println(Integer.MAX_VALUE);
     }
 
     // find maxvalue in range in array
     static int max1(int[] arr, int start, int end){
         int maxvalue = Integer.MIN_VALUE;
 
-        if (end > start){
-            return -1;
-        }
+//        if (end > start){
+//            return -1;
+//        }
         if (arr.length == 0){
             return -1;
         }
 
-        for (int i = start; i < end; i++) {
+        for (int i = start; i <= end; i++) {
             if (arr[i] > maxvalue){
                 maxvalue = arr[i];
             }
@@ -49,6 +58,41 @@ public class Question_2 {
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > maxvalue){
+                maxvalue = arr[i];
+            }
+        }
+        return maxvalue;
+    }
+
+    // find the minimum value in an array
+    static int min(int[] arr){
+        int minvalue = Integer.MAX_VALUE;
+
+        if (arr.length == 0){
+            return -1;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minvalue){
+                minvalue = arr[i];
+            }
+        }
+        return minvalue;
+    }
+
+    // find maxvalue in range in array
+    static int min1(int[] arr, int start, int end){
+        int maxvalue = Integer.MAX_VALUE;
+
+//        if (end > start){
+//            return -1;
+//        }
+        if (arr.length == 0){
+            return -1;
+        }
+
+        for (int i = start; i <= end; i++) {
+            if (arr[i] < maxvalue){
                 maxvalue = arr[i];
             }
         }
