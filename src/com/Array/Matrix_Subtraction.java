4abc9ac34df0a4a08386d0a6438f427a3dfdda0e
Ketan Parmar <kp3849668@gmail.com>
2022-11-08 09:21:38 +0530

    Matrix_Subtraction code

---
+++ src/com/Array/Matrix_Subtraction.java
@@ -0,0 +1,67 @@
+package com.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Matrix_Subtraction {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter Matrix Raw value= ");
+        int R1 = in.nextInt();
+
+        System.out.print("Enter Matrix Column value= ");
+        int C1 = in.nextInt();
+
+        int[][] arr1 = new int[R1][C1];
+
+        for (int row = 0; row < arr1.length; row++) {
+            for (int col = 0; col < arr1[row].length; col++) {
+                arr1[row][col] = (int) (Math.random()*100);
+            }
+        }
+
+        System.out.println("The First Matrix is= ");
+        for (int row = 0; row < arr1.length; row++) {
+            System.out.println(Arrays.toString(arr1[row]));
+        }
+
+        System.out.println("----------------------------------------");
+        System.out.println();
+
+        System.out.print("Enter Matrix Raw value= ");
+        int R2 = in.nextInt();
+
+        System.out.print("Enter Matrix Column value= ");
+        int C2 = in.nextInt();
+
+        int[][] arr2 = new int[R2][C2];
+
+        for (int row = 0; row < arr2.length; row++) {
+            for (int col = 0; col < arr2[row].length; col++) {
+                arr2[row][col] = (int) (Math.random()*100);
+            }
+        }
+
+        System.out.println("The Second Matrix is= ");
+        for (int row = 0; row < arr2.length; row++) {
+            System.out.println(Arrays.toString(arr2[row]));
+        }
+
+        System.out.println("----------------------------------------");
+        System.out.println();
+
+        int[][] ans = new int[R1][C2];
+
+        for (int i = 0; i < R1; i++) {
+            for (int j = 0; j < C2; j++) {
+                ans[i][j] = arr1[i][j] - arr2[i][j];
+            }
+        }
+
+        System.out.println("The Subtraction of Two Matrix is = ");
+        for (int row = 0; row < R1; row++) {
+            System.out.println(Arrays.toString(ans[row]));
+        }
+    }
+}
