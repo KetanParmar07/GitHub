219c3aef94050944f335401e7c952e3089b127c9
Ketan Parmar <kp3849668@gmail.com>
2022-11-10 22:25:51 +0530

    Sorting Algorithm

---
+++ src/com/Sorting/CyclicSort.java
@@ -0,0 +1,33 @@
+package com.Sorting;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {8,9,5,4,3,10,11,2,1,6,7};
+        Cyclic_sort(arr);
+
+        System.out.print("Sorted Array is= ");
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+   static void Cyclic_sort(int[] arr){
+        int i = 0;
+        while (i < arr.length){
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int First, int Second) {
+        int temp = arr[First];
+        arr[First] = arr[Second];
+        arr[Second] = temp;
+    }
+
+}
